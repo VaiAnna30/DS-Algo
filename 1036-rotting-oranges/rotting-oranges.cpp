30d0c5605ebe46cc790515e4379610bb3b4a244b
Vaibhav Tiwari <vaibtiwari.3003@gmail.com>
2025-07-17 19:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (11.15%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        queue<pair<pair<int,int>,int>>q;
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<int>>ans(m,vector<int>(n,0));
+        queue<pair<int,int>>q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2)q.push({{i,j},0});
-            }
-        }
-        vector<vector<int>>moves={
-            {-1,0},{1,0},{0,1},{0,-1}
-        };
-        auto check=[&](int ii,int jj){
-            if(ii<m&&ii>=0&&jj>=0&&jj<n)return true;
-            return false;
-        };
-        int ans=0;
-        while(!q.empty()){
-            int i=q.front().first.first;
-            int j=q.front().first.second;
-            int dis=q.front().second;
-            
-            ans=max(ans,dis);
-            q.pop();
-
-            for(auto &a:moves){
-                int ii=i+a[0];
-                int jj=j+a[1];
-                if(check(ii,jj)){
-                    if(grid[ii][jj]==1)q.push({{ii,jj},dis+1}),grid[ii][jj]=2;
+                if(grid[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
-
+        vector<vector<int>>move={
+            {-1,0},{1,0},{0,-1},{0,1}
+        };
+        auto valid=[&](int i,int j){
+            if(i>=0&&j>=0&&i<m&&j<n)return true;
+            return false;
+        };
+        while(!q.empty()){
+            int i=q.front().first;
+            int j=q.front().second;
+            q.pop();
+            for(auto &a:move){
+                int ii=i+a[0];
+                int jj=j+a[1];
+                if(valid(ii,jj)&&grid[ii][jj]==1){
+                    grid[ii][jj]=2;
+                    ans[ii][jj]=1+ans[i][j];
+                    q.push({ii,jj});
+                }
+            }
+        }
+        int  maxi=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1) return -1;
+                maxi=max(maxi,ans[i][j]);
             }
         }
-
-        return ans;
+        return maxi;
     }
 };
